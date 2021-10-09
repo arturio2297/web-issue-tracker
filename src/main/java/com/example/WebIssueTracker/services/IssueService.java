3b299eef3f9947e90e824f9a289873820d22a654
arturio2297 <87876173+arturio2297@users.noreply.github.com>
2021-10-09 19:06:41 +0300

    Update IssueService.java

--- src/main/java/com/example/WebIssueTracker/services/IssueService.java
+++ src/main/java/com/example/WebIssueTracker/services/IssueService.java
@@ -27,19 +27,14 @@ public class IssueService {
 
     public Page<IssueModel> searchIssues(int pageNum, String filter, String keyword) {
         Pageable pageable = PageRequest.of(pageNum - 1, PAGE_SIZE);
-        System.out.println(pageNum + " " + keyword + " " + filter);
          switch (filter) {
             case "all":
-                System.out.println(issueRepository.findByKeyword(keyword,pageable).getContent());
                 return issueRepository.findByKeyword(keyword, pageable);
             case "author":
-                System.out.println(issueRepository.findByAuthor(keyword,pageable).getContent());
                 return issueRepository.findByAuthor(keyword, pageable);
             case "name":
-                System.out.println(issueRepository.findByName(keyword,pageable).getContent());
                 return issueRepository.findByName(keyword, pageable);
         }
-        System.out.println(issueRepository.findByDescription(keyword,pageable).getContent());
         return issueRepository.findByDescription(keyword, pageable);
     }
 
